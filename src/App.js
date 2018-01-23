0b40725b89446ac742656b8b4ab36331ff90245a
Fran <fjcadenas@Frans-iMac.local>
2018-01-23 17:49:24 +0100

    Lesson 45

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Import the components
+import Person from './Person/Person';
+
+// App Component is referred as a Container because is a part of our application that can change the state of the application
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Fran', age: 26}
+        ]
+    };
+
+    // Camel case to describe de function and using the "Handler" tag to specify that this function is called to handle a certain event
+    // Handlers should be passed using the 'this.NAME.bind(this,...args)'
+    // Arrow functions may be used but they are inefficient. In this case the passed property will be a function and arguments can me included: onClick={() => this.NAME('argument')}
+    switchNameHandler = (newName) => {
+        // Don't change state like this: this.state.persons[0].name = 'Maximilian'
+        this.setState({
+            persons: [
+                {name: newName, age: 28},
+                {name: 'Manu', age: 29},
+                {name: 'Fran', age: 26}
+            ]
+        })
+    };
+
+    // Events objects are automatically passed to the handler
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: event.target.value, age: 29},
+                {name: 'Fran', age: 26}
+            ]
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button onClick={ () => this.switchNameHandler('Maximo!!')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    click={this.switchNameHandler.bind(this, 'Maximo')}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changed={this.nameChangeHandler}
+                />
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// These are the most frequent type of components that are stateless and cannot affect the state of the application
+const person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
