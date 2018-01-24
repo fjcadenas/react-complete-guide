51a693a264a6b4f920a8601501a6f944c65fd227
Fran <fjcadenas@Frans-iMac.local>
2018-01-24 15:44:07 +0100

    Section 5 Class 65

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 // Import the components
 import Person from './Person/Person';
 
@@ -8,60 +7,104 @@ import Person from './Person/Person';
 class App extends Component {
     state = {
         persons: [
-            {name: 'Max', age: 28},
-            {name: 'Manu', age: 29},
-            {name: 'Fran', age: 26}
-        ]
-    };
-
-    // Camel case to describe de function and using the "Handler" tag to specify that this function is called to handle a certain event
-    // Handlers should be passed using the 'this.NAME.bind(this,...args)'
-    // Arrow functions may be used but they are inefficient. In this case the passed property will be a function and arguments can me included: onClick={() => this.NAME('argument')}
-    switchNameHandler = (newName) => {
-        // Don't change state like this: this.state.persons[0].name = 'Maximilian'
-        this.setState({
-            persons: [
-                {name: newName, age: 28},
-                {name: 'Manu', age: 29},
-                {name: 'Fran', age: 26}
-            ]
-        })
+            {id: 'person_1', name: 'Max', age: 28},
+            {id: 'person_2', name: 'Manu', age: 29},
+            {id: 'person_3', name: 'Fran', age: 26}
+        ],
+        showPersons: false,
     };
 
     // Events objects are automatically passed to the handler
-    nameChangeHandler = (event) => {
+    nameChangeHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = {...persons[personIndex], name: event.target.value};
+
+        this.setState({ persons: persons})
+    };
+
+    deletePersonHandler = (personIndex) => {
+        // const persons = this.state.persons.slice(); // If it is an array is only a pointer, you can modify the value pointed. slice() with no arguments makes a copy so that data is not mutated
+        const persons = [...this.state.persons]; // The spread operator is the most modern way
+        persons.splice(personIndex, 1); // This removes the element in the index from the array
         this.setState({
-            persons: [
-                {name: 'Max', age: 28},
-                {name: event.target.value, age: 29},
-                {name: 'Fran', age: 26}
-            ]
+            persons: persons
+        })
+    };
+
+    togglePersonHandler = () => {
+        this.setState({
+            showPersons: !this.state.showPersons
         })
     };
 
     render() {
+
+        // should only be used on scoped elements
+        const style = {
+            backgroundColor: 'green',
+            color: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color:'black'
+            }
+        };
+
+        // Conditional rendering should be done the JS way.
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return (
+                            <Person
+                                key={person.id}
+                                click={this.deletePersonHandler.bind(this, index)}
+                                name={person.name}
+                                age={person.age}
+                                changed={(event) => this.nameChangeHandler(event, person.id)}
+                            />
+                        )
+
+                    })}
+                </div>
+            );
+
+            style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                    color:'black'
+            }
+        }
+
+        const classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red');
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push('bold');
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
-                <button onClick={ () => this.switchNameHandler('Maximo!!')}>Switch Name</button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}
-                    click={this.switchNameHandler.bind(this, 'Maximo')}
-                />
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    changed={this.nameChangeHandler}
-                />
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}
-                />
+                <p className={classes.join(' ')}>This is really working!</p>
+                <button
+                    style={style}
+                    onClick={this.togglePersonHandler}>Toggle Persons
+                </button>
+                {persons}
             </div>
         );
     }
 }
 
+// This is called a high order component. In this case we inject extra features to understand CSS
 export default App;
